eb031725c31b418c140757ac0d94bd8e172b2a7a
jjkennedy3 <54746805+jjkennedy3@users.noreply.github.com>
2021-03-13 06:42:06 -0600

    Create tab_test.rb

---
+++ tab_test.rb
@@ -0,0 +1,12 @@
+puts "this is a tab test"
+
+	#testing to see if this is a persisted change
+
+#1. create branch
+#2. edit tab settings
+#3. check in file
+#4. navigate away
+#5. navigate back and open file
+#6. validate tab spacing is still set correctly
+
+# make an edit below this to validate changes persist
